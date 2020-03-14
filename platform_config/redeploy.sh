cd93ad7d67373f43120e475c70933e9adb5fc66c
sjuhwang <sjuhwang@sjuhwang>
2020-03-15 02:01:34 +0900

    20200314

--- platform_config/redeploy.sh
+++ platform_config/redeploy.sh
@@ -1,8 +1,8 @@
-kubectl delete -f msa2-svc-member/deployment.yml
-kubectl delete -f msa2-svc-order/deployment.yml
-kubectl delete -f msa2-zuul-server/deployment.yml
-#kubectl delete -f msa2-turbine-server/deployment.yml
-#kubectl delete -f msa2-zipkin-server/deployment.yml
+#kubectl delete -f msa2-svc-member/deployment.yml
+#kubectl delete -f msa2-svc-order/deployment.yml
+#kubectl delete -f msa2-zuul-server/deployment.yml
+##kubectl delete -f msa2-turbine-server/deployment.yml
+##kubectl delete -f msa2-zipkin-server/deployment.yml
 
 docker build -t 192.168.100.102:5000/msasmp02/msa2-svc-order:1.0     msa2-svc-order/.
 docker build -t 192.168.100.102:5000/msasmp02/msa2-svc-member:1.0    msa2-svc-member/.
@@ -16,8 +16,14 @@ docker push 192.168.100.102:5000/msasmp02/msa2-zuul-server:1.0
 #docker push 192.168.100.102:5000/msasmp02/msa2-turbine-server:1.0
 #docker push 192.168.100.102:5000/msasmp02/msa2-zipkin-server:1.0
 
-kubectl apply -f msa2-svc-order/deployment.yml
-kubectl apply -f msa2-svc-member/deployment.yml
-kubectl apply -f msa2-zuul-server/deployment.yml
-#kubectl apply -f msa2-turbine-server/deployment.yml
-#kubectl apply -f msa2-zipkin-server/deployment.yml
+#kubectl apply -f msa2-svc-order/deployment.yml
+#kubectl apply -f msa2-svc-member/deployment.yml
+#kubectl apply -f msa2-zuul-server/deployment.yml
+##kubectl apply -f msa2-turbine-server/deployment.yml
+##kubectl apply -f msa2-zipkin-server/deployment.yml
+
+kubectl rollout restart deployment/msa2-svc-order
+kubectl rollout restart deployment/msa2-svc-member
+kubectl rollout restart deployment/msa2-zuul-server
+kubectl rollout restart deployment/msa2-turbine-server
+kubectl rollout restart deployment/msa2-zipkin-server
