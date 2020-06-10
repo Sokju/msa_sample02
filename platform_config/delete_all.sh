1a0c2e67a70b3818291156a2472efee3598c84a9
sjuhwang <sjuhwang@sjuhwang>
2020-06-10 19:13:45 +0900

    20200610

--- platform_config/delete_all.sh
+++ platform_config/delete_all.sh
@@ -5,9 +5,8 @@ kubectl delete -f ./msa2-svc-order/service.yml
 kubectl delete -f ./msa2-zuul-server/deployment.yml
 kubectl delete -f ./msa2-zuul-server/service.yml
 kubectl delete -f ./msa2-zuul-server/ingress.yml
-kubectl delete -f ./msa2-zipkin-server/deployment.yml
-kubectl delete -f ./msa2-zipkin-server/ingress.yml
-kubectl delete -f ./msa2-zipkin-server/service.yml
 kubectl delete -f ./msa2-turbine-server/deployment.yml
 kubectl delete -f ./msa2-turbine-server/service.yml
-kubectl delete -f ./msa2-turbine-server/ingress.yml
+kubectl delete -f ./msa2-dashboard-server/deployment.yml
+kubectl delete -f ./msa2-dashboard-server/service.yml
+kubectl delete -f ./msa2-dashboard-server/ingress.yml
